10b6c67637c458fc724116b60815cc95b4591d30
Wes Thomas <westhomas@edgecaselabs.com>
2026-01-26 14:38:44 -0600

    Explorer Snacks opens by default

--- dot_config/nvim/lua/plugins/neo-tree.lua
+++ dot_config/nvim/lua/plugins/neo-tree.lua
@@ -1,19 +1,6 @@
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-      filesystem = {
-        filtered_items = {
-          visible = true, -- Show hidden files by default
-          hide_dotfiles = false,
-          hide_gitignored = false,
-          hide_by_name = {
-            ".git",
-            "node_modules",
-            "poetry.lock",
-          },
-        },
-      },
-    },
+    enabled = false, -- Disable neo-tree, use snacks explorer instead
   },
 }
--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -15,4 +15,17 @@ return {
       },
     },
   },
+  config = function(_, opts)
+    require("snacks").setup(opts)
+
+    -- Open snacks explorer automatically when vim starts
+    vim.api.nvim_create_autocmd("VimEnter", {
+      callback = function()
+        -- Only open if no files were specified or opening a directory
+        if vim.fn.argc() == 0 or vim.fn.isdirectory(vim.fn.argv(0)) == 1 then
+          require("snacks").explorer()
+        end
+      end,
+    })
+  end,
 }
